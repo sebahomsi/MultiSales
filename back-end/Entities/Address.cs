aca21b2fb9e4458ee5e12a613b90fec14698e257
sebahomsi <45207797+sebahomsi@users.noreply.github.com>
2021-10-30 18:18:17 -0300

    Se agregan las entidades Address, Customer, Employee y Person

---
+++ back-end/Entities/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace back_end.Entities
+{
+    public class Address : BaseEntity
+    {
+        public string Country { get; set; }
+        public string Pronvice { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int StreetNumber { get; set; }
+        public bool IsApartment { get; set; }
+        public string Floor { get; set; }
+    }
+}
---
+++ back-end/Entities/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace back_end.Entities
+{
+    public class Person : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int DNI { get; set; }
+        public DateTime BirthDate { get; set; }
+        public List<Address> Addresses { get; set; }
+    }
+}
